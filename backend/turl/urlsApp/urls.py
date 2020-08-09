f2cce7abc97d2d7daa54da379deca2ad09c800b0
Patricia milou <patriciamilou97@gmail.com>
2020-08-09 12:50:08 +0300

    :sparkles: doesnt save duplicated links

--- backend/turl/urlsApp/urls.py
+++ backend/turl/urlsApp/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('<int:path_id>', views.get, name='redirect'),
+    path('', views.post, name='add'),
 ]
--- backend/turl/urlsApp/views.py
+++ backend/turl/urlsApp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import redirect
-
+import json
 from .models import Entry
 
 
@@ -21,4 +21,32 @@ def get(request, path_id):
             return JsonResponse({
                 'status_code': 404,
                 'error': 'The resource was not found'
+            })
+    elif request.method == 'POST':
+        return JsonResponse({
+            'status_code': 200,
+            'request': request
+        })
+
+
+@csrf_exempt
+def post(request):
+    if request.method == 'POST':
+        body_unicode = request.body.decode('utf-8')
+        body = json.loads(body_unicode)
+        link = body["link"]
+        try:
+            entry = Entry.objects.get(link=link)
+            return JsonResponse({
+                'status_code': 200,
+                'saved': "already exists",
+                'url': "http://127.0.0.1:8000/urls/"+entry.path
+            })
+        except Entry.DoesNotExist:
+            entry = Entry.objects.create(link=link)
+            entry.save()
+            return JsonResponse({
+                'status_code': 200,
+                'saved': "true",
+                'url': "http://127.0.0.1:8000/urls/"+entry.path
             })
