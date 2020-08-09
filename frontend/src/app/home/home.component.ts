3bdd42797309f7647e136ddde089a9a769993e6a
Patricia milou <patriciamilou97@gmail.com>
2020-08-09 22:36:25 +0300

    :sparkles: checks for input before sending a POST request

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -11,13 +11,31 @@ export class HomeComponent implements OnInit {
   faGlobeEurope = faGlobeEurope;
   link: string = '';
   short: string = null;
+  empty: boolean = null;
+  invalid: boolean = null;
   ngOnInit(): void {}
 
   convert(): void {
-    this.databaseService.convert(this.link).subscribe((res) => {
-      if (res['saved']) {
-        this.short = res['url'];
+    if (this.link.trim() !== '') {
+      var expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+      var regex = new RegExp(expression);
+      if (this.link.match(regex)) {
+        this.databaseService.convert(this.link).subscribe((res) => {
+          if (res['saved']) {
+            this.short = res['url'];
+            this.empty = null;
+            this.invalid = null;
+          }
+        });
+      } else {
+        this.empty = null;
+        this.short = null;
+        this.invalid = true;
       }
-    });
+    } else {
+      this.empty = true;
+      this.short = null;
+      this.invalid = null;
+    }
   }
 }
