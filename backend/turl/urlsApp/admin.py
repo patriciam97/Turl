367da34bb0448c2669f0dd60d808d38e0a39cb85
Patricia milou <patriciamilou97@gmail.com>
2020-08-09 12:02:22 +0300

    :sparkles: add Entry Model

--- backend/turl/urlsApp/admin.py
+++ backend/turl/urlsApp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Entry
 # Register your models here.
+
+
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ('entry_id', 'link', 'path')
+
+
+admin.site.register(Entry, EntryAdmin)
--- backend/turl/urlsApp/models.py
+++ backend/turl/urlsApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+import random
 # Create your models here.
+
+
+def random_path():
+    return str(random.randint(100, 500))
+
+
+class Entry(models.Model):
+    entry_id = models.AutoField(primary_key=True)
+    link = models.TextField(max_length=1000)
+    path = models.CharField(default=random_path, max_length=1000)
