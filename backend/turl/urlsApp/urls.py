bbafb171230e9e55641b87cd815b9a35e6dfdab9
Patricia milou <patriciamilou97@gmail.com>
2020-08-09 12:30:53 +0300

    :sparkles: add get request with redirect if resource exists

--- backend/turl/urlsApp/urls.py
+++ backend/turl/urlsApp/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('<int:path_id>', views.get, name='redirect'),
 ]
--- backend/turl/urlsApp/views.py
+++ backend/turl/urlsApp/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import redirect
+
+from .models import Entry
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the urlsApp index.")
+
+
+@csrf_exempt
+def get(request, path_id):
+    if request.method == 'GET':
+        try:
+            path_id = int(path_id)
+            entry = Entry.objects.get(path=path_id)
+            return redirect(entry.link)
+        except Entry.DoesNotExist:
+            return JsonResponse({
+                'status_code': 404,
+                'error': 'The resource was not found'
+            })
